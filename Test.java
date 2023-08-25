264f24029915d4179e3f62e62618e532ad3a0da2
Manny Corral <itsmanny06@gmail.com>
2023-08-25 10:21:27 -0700

    Created and started the test file writer class

---
+++ Test.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Test {
+    public Test() {
+        scanner = null;
+    }
+    public void write(String x) {
+        
+    }
+    public String read(File x) {
+        String text = ""
+        if (x.exists()) {
+            Scanner s = new Scanner(x);
+        }
+        rethr 
+
+    }
+}
