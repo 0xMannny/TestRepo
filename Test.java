b68f5081ba0fc0d742c715357f13032729dcb382
Sam Skulsky <sam_skulsky@icloud.com>
2023-08-29 08:37:46 -0700

    Add countCharacters()

--- Test.java
+++ Test.java
@@ -1,13 +1,18 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Test {
     public Test() {
         scanner = null;
     }
+
     public void write(String x) {
-        
+
     }
+
     public String read(File x) {
         String text = ""
         if (x.exists()) {
@@ -15,5 +20,19 @@ public class Test {
         }
         rethr 
 
+    }
+
+    public int countCharacters(String filename) throws IOException {
+        int characterCount = 0;
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+
+        while (reader.ready()) {
+            reader.read();
+            characterCount++;
+        }
+
+        reader.close();
+
+        return characterCount;
     }
 }
